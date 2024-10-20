3c9cd67bdfc30f483a0dbc4c30870d58f49c7762
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 03:20:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TituloSubtitulo from './components/TituloSubtitulo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <TituloSubtitulo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TituloSubtitulo.jsx
@@ -0,0 +1,13 @@
+function TituloSubtitulo(){
+
+    return (
+        <>
+        <h1>Olá, meu nome é Livea</h1>
+        <h2>Espero que tenha um ótimo dia!</h2>
+        </>
+    )
+
+
+}
+
+export default  TituloSubtitulo
